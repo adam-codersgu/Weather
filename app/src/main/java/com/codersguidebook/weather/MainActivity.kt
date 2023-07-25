cfb530d8296e9e312feeca9f982abb015d725e46
Adam Hawke <ah14275@my.bristol.ac.uk>
2023-07-25 13:20:57 +0100

    Added OpenWeather details

--- app/src/main/java/com/codersguidebook/weather/MainActivity.kt
+++ app/src/main/java/com/codersguidebook/weather/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.codersguidebook.weather
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        // TODO: Get your API key here https://home.openweathermap.org/api_keys
+        //  Note it can sometimes take a couple of hours following email confirmation for API keys to become active
+        private const val API_KEY = "INSERT-API-KEY-HERE"
+        private const val CITY_NAME_URL = "https://api.openweathermap.org/data/2.5/weather?q="
+        private const val GEO_COORDINATES_URL = "https://api.openweathermap.org/data/2.5/weather?lat="
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
