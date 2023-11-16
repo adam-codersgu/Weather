ec90093df8d520e1dedb4b99fa4e49b4108cd5cb
Adam Hawke <ah14275@my.bristol.ac.uk>
2023-11-16 16:17:09 +0000

    Import update

--- app/src/main/java/com/codersguidebook/weather/MainActivity.kt
+++ app/src/main/java/com/codersguidebook/weather/MainActivity.kt
@@ -27,7 +27,8 @@ import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 import java.text.DateFormat
-import java.util.*
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
